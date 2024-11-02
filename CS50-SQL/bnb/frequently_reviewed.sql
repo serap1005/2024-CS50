c562ae2178eeb61d167156f1cd6a43e76973b2e5
serap1005 <serawpsl1005@gmail.com>
2024-11-02 11:22:20 +0900

    Add files via upload

---
+++ CS50-SQL/bnb/frequently_reviewed.sql
@@ -0,0 +1,6 @@
+CREATE VIEW "frequently_reviewed" AS
+SELECT "listings"."id", "property_type", "host_name", COUNT("reviews"."id") AS "reviews"
+FROM "listings"
+JOIN "reviews" ON "listings"."id" = "reviews"."listing_id"
+GROUP BY "listings"."id"
+ORDER BY "reviews" DESC, "property_type", "host_name";
---
+++ CS50-SQL/bnb/june_vacancies.sql
@@ -0,0 +1,6 @@
+CREATE VIEW "june_vacancies" AS
+SELECT "listings"."id", "property_type", "host_name", COUNT("available") AS "days_vacant"
+FROM "listings"
+JOIN "availabilities" ON "listings"."id" = "availabilities"."listing_id"
+WHERE "available" = 'TRUE' AND ("date" BETWEEN '2023-06-01' AND '2023-06-30')
+GROUP BY "listings"."id"
