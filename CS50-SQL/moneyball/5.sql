aca0bbe260a7567cd8ef8429a34e1ca407a0def7
serap1005 <serawpsl1005@gmail.com>
2024-11-02 11:18:14 +0900

    Add files via upload

---
+++ CS50-SQL/moneyball/5.sql
@@ -0,0 +1,7 @@
+SELECT "name" FROM "teams"
+JOIN "performances" ON "performances"."team_id"="teams"."id"
+WHERE "player_id"=(
+    SELECT "id" FROM "players"
+    WHERE "first_name" = 'Satchel' AND "last_name"='Paige'
+)
+GROUP BY "name";
---
+++ CS50-SQL/players/7.sql
@@ -0,0 +1,8 @@
+SELECT COUNT(*) FROM "players"
+WHERE
+    (
+		"bats" = 'L' AND "throws" = 'R'
+	)
+	OR (
+		"bats" = 'R' AND "throws" = 'L'
+	);
